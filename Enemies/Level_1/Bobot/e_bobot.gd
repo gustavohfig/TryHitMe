0aec01939a449e68fccb909d68498f693c66f843
Gustavo Heitmann Freire Figueiredo <132158933+gustavohfig@users.noreply.github.com>
2024-06-15 21:36:53 -0300

    Second Commit

---
+++ Enemies/Level_1/Bobot/e_bobot.gd
@@ -0,0 +1,77 @@
+extends CharacterBody2D
+
+#BUG: Se inimigo enconstar no jogador 0.1sec antes da função hurt() ser chamada, inimigo causa 20 de dano!!!
+
+@onready var player = get_node("/root/Arena/Player")
+
+var health = 60
+var dmg = 10
+var dmg_rate = 0.5
+var scoreValue = 10
+var contato_player = false
+
+# Parameters for separation logic
+var desired_distance = 80.0  # Distance to maintain from the player
+var separation_strength = 200.0  # Strength of the separation force
+
+func _ready():
+	$Bobot.play_walk_animation()
+
+
+@warning_ignore("unused_parameter")
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 250
+
+	# Apply separation force if too close to the player
+	var to_player = global_position.direction_to(player.global_position)
+	var distance_to_player = global_position.distance_to(player.global_position)
+	
+	if distance_to_player < desired_distance:
+		var separation_force = to_player.normalized() * -separation_strength
+		velocity += separation_force
+
+	move_and_slide()
+	walk_face()
+	die()
+
+func walk_face():
+	
+	if velocity.x > 0:
+		$Bobot.scale.x = -1  # Inverte o nó horizontalmente
+	else:
+		$Bobot.scale.x = 1
+
+func take_damage(weapon_dmg):
+	health -= weapon_dmg
+	$Bobot.play_hurt()
+
+func die():
+	if health <= 0:
+		Score.atualizarScore(scoreValue)
+		queue_free()
+		# Add animation to being hurt
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
+
+
+func hurt():
+	if contato_player == true:
+		player.life -= dmg
+
+# SIGNALS
+
+# Verifica se algo enconstou na Hurtbox (Area2D)
+func _on_hurtbox_body_entered(body):
+	if body == player:
+		contato_player = true
+		hurt()
+
+func _on_hurtbox_body_exited(body):
+	if body == player:
+		contato_player = false
+
+func _on_dmg_rate_timeout():
+	hurt()
---
+++ Player/player.gd
@@ -0,0 +1,95 @@
+extends CharacterBody2D
+
+#Sounds:
+
+#------------------------------------------------------------------------
+
+
+var life = 200.0
+var walking = false
+
+# Preload your weapon scenes
+var weapon1_scene = preload("res://Weapons/Glock/pistol.tscn")
+var weapon2_scene = preload("res://Weapons/Rifle/rifle.tscn")
+var weapon3_scene = preload("res://Weapons/Shotgun/shotgun.tscn")
+
+# Variable to hold the current weapon node
+var current_weapon = null
+var current_weapon_index = 1
+var ammo_counts = {1: 17, 2: 20, 3: 6}  # Default ammo counts for each weapon
+
+func _ready():
+	switch_weapon(1)
+
+func _input(event):
+	if event.is_action_pressed("switch_weapon_1"):
+		switch_weapon(1)
+	elif event.is_action_pressed("switch_weapon_2"):
+		switch_weapon(2)
+	elif event.is_action_pressed("switch_weapon_3"):
+		switch_weapon(3)
+	
+	if event.is_action_pressed("shoot") and current_weapon:
+		current_weapon.shoot()
+	if event.is_action_pressed("reload") and current_weapon:
+		current_weapon.reload()
+
+func switch_weapon(weapon_number):
+	save_current_ammo()
+	
+	if current_weapon:
+		current_weapon.queue_free()
+	
+	match weapon_number:
+		1:
+			current_weapon = weapon1_scene.instantiate()
+		2:
+			current_weapon = weapon2_scene.instantiate()
+		3:
+			current_weapon = weapon3_scene.instantiate()
+	
+	if current_weapon:
+		add_child(current_weapon)
+		current_weapon.position = Vector2.ZERO
+		current_weapon.ammo = ammo_counts[weapon_number]
+		current_weapon_index = weapon_number
+	
+	# Update the weapon image in the HUD
+	$Hud.update_weapon_img(weapon_number)
+
+func save_current_ammo():
+	if current_weapon:
+		ammo_counts[current_weapon_index] = current_weapon.ammo
+
+func _physics_process(_delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * 500
+	move_and_slide()
+	
+	walk_face()
+	hud()
+	
+	
+
+func walk_face():
+	if velocity.length() > 0.0:
+		$Null_C.play_walk_animation()
+	else:
+		$Null_C.play_idle_animation()
+	
+	if get_global_mouse_position().x < global_position.x:
+		$Null_C.scale.x = -1
+		if current_weapon:
+			current_weapon.scale.x = -1
+	else:
+		$Null_C.scale.x = 1
+		if current_weapon:
+			current_weapon.scale.x = 1
+
+func hud():
+	if current_weapon:
+		$Hud.update_ammo(current_weapon.ammo, current_weapon.max_ammo)
+	$Hud.update_healthbar(life)
+
+func take_damage(_dmg):
+	pass
